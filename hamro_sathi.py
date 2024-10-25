5fb1c4784998a3c03189ef6f9e42b462cd052958
sujit7778 <sujitkumardas7778@gmail.com>
2024-10-25 07:33:31 +0545

    project is workig properly

---
+++ hamro_sathi.py
@@ -0,0 +1,20 @@
+## Sujit kharch 
+## dipesh kharch
+## shyam kharch
+
+
+sujit = int(input("Enter the Sujit kharch = "))
+shyam =int(input("Enter the shyam kharch = "))
+dipesh = int(input(" Enter the dipesh kharch ="))
+
+output =(sujit + shyam + dipesh)//3
+
+print("Each person will spend = ",output)
+
+a=(output - sujit)
+b=(output - shyam)
+c=(output - dipesh)
+print("Sujit should pay =",a)
+print("Shyam should pay=",b)
+print("Dipesh should pay =",c)
+
